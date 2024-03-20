757d1a182711a74350f189f16c05da883408e7cb
Felipe AV <felipealcantaravieira@hotmail.com>
2024-03-20 12:26:48 -0300

    sections until testimonials completed

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { feedback } from '../constants';
+import styles from '../style';
+import FeedbackCard from './FeedbackCard';
 
-const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
+const Testimonials = () => (
+    <section id="clients" className={`${styles.paddingY} ${styles.fle}`}></section>
   )
-}
 
 export default Testimonials
